92fa5034ed33a47f718908d59db2f09fedecbf92
sridharsilver <sridharsilver@gmail.com>
2026-05-20 00:53:17 +0530

    Fix blank page runtime error by importing useMemo in Fees.jsx

--- src/pages/Admin/Fees.jsx
+++ src/pages/Admin/Fees.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import { PageTitle } from "../../components/PageTitle";
 import { DataTable } from "../../components/DataTable";
